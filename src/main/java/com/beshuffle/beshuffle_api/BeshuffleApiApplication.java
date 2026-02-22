ceaa33e506fecd85058a2a4526951f78866d0ae4
perondi <guilhermeperondi09@gmail.com>
2026-02-22 14:04:19 -0300

    feat: first commit

---
+++ src/main/java/com/beshuffle/beshuffle_api/BeshuffleApiApplication.java
@@ -0,0 +1,13 @@
+package com.beshuffle.beshuffle_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeshuffleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeshuffleApiApplication.class, args);
+	}
+
+}
